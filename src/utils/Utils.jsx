b6d1981b18c96b349504d575e3b50e2d8297e61a
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2024-01-25 22:28:51 -0500

    add one img

--- src/utils/Utils.jsx
+++ src/utils/Utils.jsx
@@ -1,7 +1,7 @@
 import imgPuraVidaViajes from '../images/puraVidaViajes.webp'
 import imgEsquinaVerdePlanta from '../images/EsquinaVerde.webp'
 import imgDriversPi from '../images/DriversPI.jpg'
-
+import imgUserAuthApp from '../images/UserAuthApp.jpg'
 
 export const Projects = [
 
@@ -32,6 +32,7 @@ export const Projects = [
     {
         id: 4,
         name: "User Auth App",
+        img: imgUserAuthApp,
         demo: "https://userauthappescorihuela.netlify.app",
         repository: "https://github.com/JhonattanEscorihuela/UserAuthApp",
         despription: "A user authentication web app built with React.js, Redux, and CSS. It uses JWT tokens for authentication, Axios for API requests, and is deployable on Netlify and Render. Securely manage user registration and authentication!"
